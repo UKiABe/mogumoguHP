7d09673a6c89da6ca12865cf1298f076915274e5
UkiAbe <unlimited.blade.works22@gmail.com>
2019-02-09 14:33:39 +0900

    css,javascriptファイルの移植完了

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
+//= require popper.min
+//= require bootstrap
+//= require mdb
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
