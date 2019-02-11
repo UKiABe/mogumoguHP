018b59b0b22e5a3149776826af78a61638916220
UkiAbe <unlimited.blade.works22@gmail.com>
2019-02-11 22:00:18 +0900

    HPとブログページのCSSを出し分ける

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module MogmoguHP
     config.load_defaults 5.2
     config.assets.paths << Rails.root.join("app", "assets", "font")
     config.assets.paths << Rails.root.join("vendor", "assets", "img")
+    config.assets.precompile += %w( blog/clean-blog.min.css )
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
